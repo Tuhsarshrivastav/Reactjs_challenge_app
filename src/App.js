5c26bd0d42a6c266156c951fb1e3f9df8d9fe550
Tuhar Shrivastav <ts3657@gmail.com>
2022-02-05 13:23:00 +0530

    added dashboard content section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+let adjustInterval = undefined;
 function App() {
+  const [watch, setWatch] = useState(0);
+  const [started, setStarted] = useState(false);
+  const startWatch = () => {
+    adjustInterval = setInterval(() => {
+      setWatch((x) => x + 1);
+    }, 1000);
+    setStarted(true);
+  };
+  const stopWatch = () => {
+    clearInterval(adjustInterval);
+    setStarted(false);
+  };
+
+  const resatWatch = () => {
+    setWatch(0);
+    clearInterval(adjustInterval);
+    setStarted(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <h1>Start Stop watch</h1>
+        <h3>{watch}</h3>
+        <div className="buttonclass">
+          <button
+            style={{ background: "green" }}
+            disabled={started}
+            onClick={startWatch}
+          >
+            Start
+          </button>
+          <button style={{ background: "red" }} onClick={stopWatch}>
+            Stop
+          </button>
+          <button
+            style={{ background: "yellow", color: "black" }}
+            onClick={resatWatch}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
